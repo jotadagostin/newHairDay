fb134172222a9acd6052560580ccf2cd5ab0a648
João Paulo Dagostin <dagostinjota@gmail.com>
2025-12-18 12:22:45 -0300

    setting index

---
+++ hairday/src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-yellow-400 text-[var(--font-title-lg-size)]">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
