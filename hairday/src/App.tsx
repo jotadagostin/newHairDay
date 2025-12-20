e525c3ef5f2a17bf9f5f638c5d3e10623660b516
João Paulo Dagostin <dagostinjota@gmail.com>
2025-12-19 22:47:10 -0300

    starting new icomponent
    TimeSelect

--- hairday/src/App.tsx
+++ hairday/src/App.tsx
@@ -1,15 +1,17 @@
 import Button from "./components/Button";
 import TextInput from "./components/TextInput";
+import { TimeSelect } from "./components/TimeSelect";
 import TrashButton from "./components/TrashButton";
 
 import "./index.css";
 
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-5">
       <TrashButton />
       <Button />
       <TextInput />
+      <TimeSelect />
     </div>
   );
 }
---
+++ hairday/src/components/TimeSelect.tsx
@@ -0,0 +1,11 @@
+export function TimeSelect() {
+  return (
+    <div>
+      <input
+        type="time"
+        className="border bg-gray-500 text-gray-200 border-black rounded-lg px-5 py-1  "
+        defaultValue="09:00"
+      />
+    </div>
+  );
+}
